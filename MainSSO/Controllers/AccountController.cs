42ed122dd2641502adf6b2407296e3d4e392970b
SChervonyi <bekyts@gmail.com>
2015-05-12 09:15:24 +0300

    initial commit

---
+++ MainSSO/Controllers/AccountController.cs
@@ -0,0 +1,63 @@
+﻿using MainSSO.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using WebMatrix.WebData;
+
+namespace MainSSO.Controllers
+{
+    [Authorize]
+    [InitializeSimpleMembership]
+    public class AccountController : Controller
+    {
+        [AllowAnonymous]
+        public ActionResult Login(string returnUrl)
+        {
+            if (Request.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(string username, string password, string returnUrl)
+        {
+            //WebSecurity.Logout();
+
+            if (ModelState.IsValid && WebSecurity.Login(username, password, false))
+            {
+                if (!string.IsNullOrEmpty(returnUrl))
+                {
+                    return Redirect(returnUrl);
+                }
+                else
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Invalid login details");
+                ViewBag.ReturnUrl = returnUrl;
+                return View();
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult LogOff()
+        {
+            WebSecurity.Logout();
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
