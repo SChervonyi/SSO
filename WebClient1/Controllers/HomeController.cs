4a373fddb41aa887dc70e60bca0e9977e5b66ef0
SChervonyi <SChervonyi@luxoft.com>
2015-05-12 15:14:06 +0300

    use roles

--- WebClient1/Controllers/HomeController.cs
+++ WebClient1/Controllers/HomeController.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebClient1.Filters;
 
 namespace WebClient1.Controllers
 {
     [Authorize]
+	[InitializeSimpleMembership]
     public class HomeController : Controller
     {
         // GET: Home
